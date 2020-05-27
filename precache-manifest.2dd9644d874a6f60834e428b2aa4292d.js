6dd4e0b5186072cdf2f8ffa2642040587e799f1c
nomen1 <olesiamakarenko5115@gmail.com>
2020-05-27 14:43:54 +0300

    Updates

---
+++ precache-manifest.2dd9644d874a6f60834e428b2aa4292d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cf3eb7a62e19c3c480c75bf6795ff3",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "0b82ba12f3205383f339",
+    "url": "/chat/static/css/main.ababf2ac.chunk.css"
+  },
+  {
+    "revision": "7143c80a5eddb7dcf8ba",
+    "url": "/chat/static/js/2.cdd90a54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chat/static/js/2.cdd90a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b82ba12f3205383f339",
+    "url": "/chat/static/js/main.b9ad1f30.chunk.js"
+  },
+  {
+    "revision": "da19fe80d353b604b10e",
+    "url": "/chat/static/js/runtime-main.fc59ca1d.js"
+  }
+]);
